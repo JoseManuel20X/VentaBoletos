a5c34bda4523ae08d8c643c4807aacf4a78e83c9
JoseManuel20X <josemmg2106@gmail.com>
2024-10-13 11:15:28 -0900

    Creacion del repositorio

---
+++ Boleteria/src/boleteria/Boleteria.java
@@ -0,0 +1,20 @@
+package boleteria;
+
+/**
+ *
+ * @author Manuel
+ */
+public class Boleteria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+    }
+    
+}
